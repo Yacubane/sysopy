5c2e253ba878a809d98c56fbb8c119e2aaafa271
Jakub Dybczak <jakub.dybczak@gmail.com>
2019-03-12 19:19:53 +0100

    Done

--- cw01/zad2/reporting.c
+++ cw01/zad2/reporting.c
@@ -6,7 +6,6 @@
 #include <time.h>
 #include <sys/times.h>
 
-
 static int reporting_status = 0;
 static int fd;
 
@@ -21,7 +20,7 @@ static int create_error(char* message) {
 int start_reporting(char* filename, char* title, int append)
 {
     if(reporting_status==1)
-        return create_error("Reporting is already started");
+        return create_error("Reporting already started");
 
     int flag = 0;
     if(append == 1)
@@ -31,19 +30,26 @@ int start_reporting(char* filename, char* title, int append)
 
     if((fd = open(filename, O_WRONLY | O_CREAT | flag, 0644)) >= 0) 
     {
-        char buffer[255]; 
-
         reporting_status = 1;
 
+        char buffer[255]; 
+
         sprintf(buffer, "%s\n", title);
-        if(add_report_text(buffer) < 0) 
+        if(add_report_text(buffer) < 0)  {
+            reporting_status = 0;
+            close(fd);
             return create_error("Cannot add first line to report");
+        }
 
         sprintf(buffer, "%-20s %9s %9s %9s %9s %9s %9s\n",
                 "Measurement", "Real[s]", "Real[n]", 
                 "utime[s]", "stime[s]", "cutime[s]", "cstime[s]");
-        if(add_report_text(buffer) < 0)
+        if(add_report_text(buffer) < 0)  {
+            reporting_status = 0;
+            close(fd);
             return create_error("Cannot add second line to report");
+        }
+
 
         return 0;
     } else
@@ -68,7 +74,7 @@ int add_report_text(char* content) {
 
 int stop_reporting() {
     if(reporting_status==0)
-        return create_error("Reporting is already stopped");
+        return create_error("Reporting already stopped");
 
     add_report_text("\n");
 
@@ -79,26 +85,28 @@ int stop_reporting() {
 }
 
 
-struct timespec diff(struct timespec start, struct timespec stop)
+struct timespec timespec_diff(struct timespec start, struct timespec stop)
 {
 	struct timespec temp;
-	if ((stop.tv_nsec-start.tv_nsec)<0) {
-		temp.tv_sec = stop.tv_sec-start.tv_sec-1;
-		temp.tv_nsec = 1000000000+stop.tv_nsec-start.tv_nsec;
-	} else {
-		temp.tv_sec = stop.tv_sec-start.tv_sec;
-		temp.tv_nsec = stop.tv_nsec-start.tv_nsec;
+	if ((stop.tv_nsec-start.tv_nsec)<0) 
+    {
+		temp.tv_sec = stop.tv_sec - start.tv_sec - 1;
+		temp.tv_nsec = 1000000000 + stop.tv_nsec-start.tv_nsec;
+	} else 
+    {
+		temp.tv_sec = stop.tv_sec - start.tv_sec;
+		temp.tv_nsec = stop.tv_nsec -  start.tv_nsec;
 	}
 	return temp;
 }
 
-struct tms diff_tms(struct tms start, struct tms end) {
+struct tms tms_diff(struct tms start, struct tms end) 
+{
     struct tms temp;
     temp.tms_cstime = end.tms_cstime - start.tms_cstime;
     temp.tms_cutime = end.tms_cutime - start.tms_cutime;
     temp.tms_stime = end.tms_stime - start.tms_stime;
     temp.tms_utime = end.tms_utime - start.tms_utime;
-
     return temp;
 }
 
@@ -108,24 +116,24 @@ void start_report_timer()
     clock_gettime(CLOCK_REALTIME, &timespec_start);
 }
 
-void stop_report_timer(char* text)
+int stop_report_timer(char* text)
 {
     times(&tms_stop);
-    struct tms tms_diff_time = diff_tms(tms_start, tms_stop);
-
     clock_gettime(CLOCK_REALTIME, &timespec_stop);
-    struct timespec timespec_diff_time = diff(timespec_start, timespec_stop);
+
+    struct tms tms_diff_time = tms_diff(tms_start, tms_stop);
+    struct timespec timespec_diff_time = timespec_diff(timespec_start, timespec_stop);
 
     long clktck = sysconf(_SC_CLK_TCK);
 
     char buffer[255]; 
 
     sprintf(buffer, "%-20s %9ld %09ld %9.2f %9.2f %9.2f %9.2f\n",
-            text,timespec_diff_time.tv_sec, timespec_diff_time.tv_nsec, 
+            text, timespec_diff_time.tv_sec, timespec_diff_time.tv_nsec, 
             tms_diff_time.tms_utime / (double) clktck,
             tms_diff_time.tms_stime / (double) clktck,
             tms_diff_time.tms_cutime / (double) clktck,
             tms_diff_time.tms_cstime / (double) clktck);
 
-    add_report_text(buffer);
+    return add_report_text(buffer);
 }
--- cw01/zad2/reporting.h
+++ cw01/zad2/reporting.h
@@ -2,8 +2,7 @@
 
 int start_reporting(char* filename, char* title, int append);
 int add_report_text(char* content);
-void add_first_reporting_line();
 void start_report_timer();
-void stop_report_timer(char* text);
+int stop_report_timer(char* text);
 int stop_reporting();
 
