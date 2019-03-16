0ef57436e35381dd2c58b01ced18fd697bb7640c
Jakub Dybczak <jakub.dybczak@gmail.com>
2019-03-16 14:58:29 +0100

    Better error handling

--- cw02/zad2/files_dir.c
+++ cw02/zad2/files_dir.c
@@ -8,12 +8,18 @@
 #include <string.h>
 #include <time.h>
 
-static int create_error(char* message) 
+static int create_error(char *message) 
 {
     fprintf(stderr, "%s\n", message);
     return -1;
 }
-char* get_tile_type(struct stat *stat_buffer) 
+
+static int create_error_and_close_dir(DIR *dir, char *message) 
+{
+    closedir(dir);
+    return create_error(message);
+}
+static char* get_tile_type(struct stat *stat_buffer) 
 {
     if(S_ISREG(stat_buffer->st_mode)) return "file";
     if(S_ISDIR(stat_buffer->st_mode)) return "dir";
@@ -35,7 +41,7 @@ int readdir_recursive(struct stat *stat_buffer, int buffer_size, int sign, time_
     while((file = readdir(dir)) != NULL) {
         if(strcmp(file->d_name, ".") && strcmp(file->d_name, ".."))
         {
-            if(lstat(file->d_name, stat_buffer) < 0) return create_error("stat error");
+            if(lstat(file->d_name, stat_buffer) < 0) return create_error_and_close_dir(dir, "stat error");
             int print = 1;
             if(sign == -1)
                 if(stat_buffer->st_mtime/60 >= time/60) print=0;
@@ -55,16 +61,18 @@ int readdir_recursive(struct stat *stat_buffer, int buffer_size, int sign, time_
                 strftime (mtime_buffer,50,"%Y-%m-%d %H:%M",mtime);
 
                 char cwd_buffer[255];
-                if(getcwd(cwd_buffer, 255) == NULL) return create_error("getcwd error");
+                if(getcwd(cwd_buffer, 255) == NULL) return create_error_and_close_dir(dir, "getcwd error");
 
                 printf("%10s %11ldB %16s %16s %s/%s\n", get_tile_type(stat_buffer),
                 stat_buffer->st_size, atime_buffer, mtime_buffer, cwd_buffer, file->d_name);
             }
             
             if(S_ISDIR(stat_buffer->st_mode)){
-                if(chdir(file->d_name) != 0) return create_error("chdir error 1");
+                if(chdir(file->d_name) != 0) 
+                    return create_error_and_close_dir(dir, "Cannot get forward in files tree");
                 readdir_recursive(stat_buffer, buffer_size, sign, time);
-                if(chdir("..") != 0) return create_error("chdir error 2");
+                if(chdir("..") != 0) 
+                    return create_error_and_close_dir(dir, "Cannot get back in files tree");
             }
         }
 
@@ -83,7 +91,7 @@ int view_dir(char *path, int sign, time_t time)
 
     struct stat *stat_buffer = malloc(sizeof(struct stat));
 
-    if(chdir(path) != 0) return create_error("chdir error 0");
+    if(chdir(path) != 0) return create_error("Cannot change directory to choosen path");
     readdir_recursive(stat_buffer, 255, sign, time);
     return 0;
 }
