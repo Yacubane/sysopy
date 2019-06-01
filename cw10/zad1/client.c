8e0973148b1d7841ef3321cf5594f35170fdc8f9
jakubdybczak <jakub.dybczak@gmail.com>
2019-06-02 01:31:03 +0200

    Add working TCP (spaghetti code alert)

--- cw10/zad1/client.c
+++ cw10/zad1/client.c
@@ -1,7 +1,10 @@
 #include <arpa/inet.h>
+#include <ctype.h>
 #include <errno.h>
+#include <math.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,11 +13,47 @@
 #include <sys/un.h>
 #include <unistd.h>
 
+#include "errors.h"
+#include "queue.h"
+#include "utils.h"
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
 #define PORT 5555
 #define MAXMSG 512
 
-void unix_tcp_test() {
-  int sock1;
+pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t queue_new_element_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t socket_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t socket_send_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int sock1;
+
+typedef struct request_t {
+  int id;
+  char* content;
+  int size;
+} request_t;
+
+typedef struct word_node_t word_node_t;
+typedef struct word_node_t {
+  char* text;
+  int text_size;
+  int text_count;
+  word_node_t* next;
+
+} word_node_t;
+
+typedef struct word_counter_t {
+  word_node_t* first;
+  int size;
+
+} word_counter_t;
+
+queue_t requests;
+
+void unix_tcp_test(char* path) {
   struct sockaddr_un name;
 
   /* Create the socket. */
@@ -26,33 +65,15 @@ void unix_tcp_test() {
 
   /* Give the socket a name. */
   name.sun_family = AF_UNIX;
-  strcpy(name.sun_path, "/tmp/socket");
+  strcpy(name.sun_path, path);
 
   if (connect(sock1, (struct sockaddr*)&name, sizeof(name))) {
-    perror("Connection failed");
+    perror("Connection failed2");
   }
 
-  char buffer[512];
-  char* hello = "Hello from client";
-
-  int a = sendto(sock1, hello, strlen(hello), 0, NULL, 0);
-
-  printf("Hello message sent.%d %d\n", a, errno);
-  perror("Aha");
-
-  int n;
-  socklen_t len;
-  n = recvfrom(sock1, (char*)buffer, 512, MSG_WAITALL, (struct sockaddr*)&name,
-               &len);
-  buffer[n] = '\0';
-  printf("Server : %s\n", buffer);
-
-  close(sock1);
 }
 
-
 void unix_udp_test() {
-  int sock1;
   struct sockaddr_un name;
 
   /* Create the socket. */
@@ -66,7 +87,6 @@ void unix_udp_test() {
   name.sun_family = AF_UNIX;
   strcpy(name.sun_path, "/tmp/socket");
 
-
   char buffer[512];
   char* hello = "Hello from client";
 
@@ -84,9 +104,7 @@ void unix_udp_test() {
   close(sock1);
 }
 
-void inet_tcp_test() {
-  int sock1;
-
+void inet_tcp_test(char* address, int port) {
   struct sockaddr_in name;
 
   /* Create the socket. */
@@ -98,32 +116,14 @@ void inet_tcp_test() {
 
   /* Give the socket a name. */
   name.sin_family = AF_INET;
-  name.sin_port = htons(PORT);
-  name.sin_addr.s_addr = htonl(INADDR_ANY);
-
+  name.sin_port = htons(port);
+  inet_pton(AF_INET, address, &(name.sin_addr));
   if (connect(sock1, (struct sockaddr*)&name, sizeof(name))) {
     perror("Connection failed");
   }
-
-  char buffer[512];
-  char* hello = "Hello from client";
-
-  sendto(sock1, hello, strlen(hello), 0, (struct sockaddr*)&name, sizeof(name));
-
-  printf("Hello message sent.\n");
-  int n;
-  socklen_t len;
-  n = recvfrom(sock1, (char*)buffer, 512, MSG_WAITALL, (struct sockaddr*)&name,
-               &len);
-  buffer[n] = '\0';
-  printf("Server : %s\n", buffer);
-
-  close(sock1);
 }
 
 void inet_udp_test() {
-  int sock1;
-
   struct sockaddr_in name;
 
   /* Create the socket. */
@@ -154,12 +154,217 @@ void inet_udp_test() {
                &len);
   buffer[n] = '\0';
   printf("Server : %s\n", buffer);
+  printf("Server\n");
 
   close(sock1);
 }
+#define MSGTYPE_PING 1
+#define MSGTYPE_RESULT 2
+#define MSGTYPE_REQUEST 3
+
+int init_word_counter(word_counter_t* counter) {
+  counter->first = malloc(sizeof(word_node_t));
+  counter->first->next = NULL;
+  counter->size = 0;
+  return 0;
+}
+int add_word_counter(word_counter_t* counter, char* word, int size) {
+  for (int i = 0; i < size; i++)
+    word[i] = tolower(word[i]);
+
+  word_node_t* node = counter->first->next;
+  int found = 0;
+
+  while (node != NULL) {
+    if (strncmp(word, node->text, MIN(size, node->text_size)) == 0) {
+      found = 1;
+      node->text_count++;
+      break;
+    }
+    node = node->next;
+  }
+  if (!found) {
+    word_node_t* new_node = malloc(sizeof(word_node_t));
+    new_node->text_count = 1;
+    new_node->text_size = size;
+    new_node->text = word;
+
+    new_node->next = counter->first->next;
+    counter->first->next = new_node;
+    counter->size++;
+  }
+}
+
+void free_request(request_t* request) {
+  // todo
+}
+void* request_handler_fun(void* data) {
+  while (1) {
+    pthread_mutex_lock(&queue_mutex);
+    request_t* request;
+    while ((request = queget(&requests)) == NULL)
+      pthread_cond_wait(&queue_new_element_cond, &queue_mutex);
+    pthread_mutex_unlock(&queue_mutex);
+
+    printf("Wyciagam z kolejki\n");
+
+    word_counter_t word_counter;
+    init_word_counter(&word_counter);
+
+    int words_count = 0;
+    int state = 1;
+    for (int j = 0; j < request->size; j++) {
+      char ch = request->content[j];
+      if (ch == '\t' || ch == ' ' || ch == '\n' || ch == '\r') {
+        state = 1;
+      } else if (ch == '(') {
+      } else {
+        if (state == 1) {
+          words_count++;
+          int k = j + 1;
+          while (k < request->size) {
+            char ch = request->content[k];
+            if (ch == '\t' || ch == ' ' || ch == '\n' || ch == '\r' ||
+                ch == '.' || ch == ',' || ch == ':' || ch == ';' || ch == ')' ||
+                ch == '?' || ch == '!') {
+              break;
+            }
+            k++;
+          }
+          if (k - j > 0)
+            add_word_counter(&word_counter, request->content + j, k - j);
+        }
+        state = 0;
+      }
+    }
+
+    pthread_mutex_lock(&socket_send_mutex);
+    int type = 2;
+    send(sock1, &type, 1, 0);
+    send(sock1, &request->id, sizeof(int), 0);
+    send(sock1, &words_count, sizeof(int), 0);
+
+    int output_size = 0;
+    word_node_t* node = word_counter.first->next;
+    while (node != NULL) {
+      output_size += node->text_size;
+      output_size += 2;  //: space
+      output_size += 1;  // \n
+      int n_digits = floor(log10(abs(node->text_count))) + 1;
+      output_size += n_digits;
+      node = node->next;
+    }
+    output_size += 1;  // \0
+
+    char buffer[128];
+
+    char* output = malloc(sizeof(char) * output_size);
+    output[0] = '\0';
+    node = word_counter.first->next;
+
+    printf("test1 %s\n", output);
+
+    while (node != NULL) {
+      snprintf(buffer, 128, ": %d\n", node->text_count);
+      strncat(output, node->text, node->text_size);
+      strcat(output, buffer);
+      node = node->next;
+    }
+
+    printf("test2 %s %d\n", output, output_size);
+
+    send(sock1, &output_size, sizeof(int), 0);
+    int pointer = 0;
+    while (pointer < output_size) {
+      int size = 1024;
+      if (pointer + size > output_size) {
+        size = output_size - pointer;
+      }
+      send(sock1, output + pointer, size, 0);
+      pointer += size;
+    }
+
+    pthread_mutex_unlock(&socket_send_mutex);
+
+    free_request(request);
+  }
+}
+
+int inet = 0;
+int port = 0;
 
 int main(int argc, char* argv[]) {
-  unix_udp_test();
+  if (argc != 4 && argc != 5)
+    return err(
+        "Missing arguments <name> <mode:UNIX/INET> <addr:IP/path> <port>", -1);
+
+  if (strncmp(argv[2], "INET", 4) == 0)
+    inet = 1;
+  else if (strncmp(argv[2], "UNIX", 4) == 0)
+    inet = 0;
+  else
+    return err("Unknown mode", -1);
+
+  if (inet) {
+    if (parse_int(argv[4], &port) < 0)
+      return err("Wrong port", -1);
+  }
+
+  quenew(&requests);
+
+  pthread_t id;
+  int data = 0;
+  int res = pthread_create(&id, NULL, request_handler_fun, &data);
+  if (inet) {
+    inet_tcp_test(argv[3], port);
+  } else {
+    unix_tcp_test(argv[3]);
+  }
+  send(sock1, argv[1], strlen(argv[1]), 0);
+
+  int BUFFER_SIZE = 512;
+  char buffer[512];
+  char* hello = "Hello from client";
+
+  printf("Hello message sent.\n");
+  int n;
+  socklen_t len;
+  while (1) {
+    char msgtype;
+    n = recv(sock1, &msgtype, 1, 0);
+    if (n <= 0) {
+      return -1;
+    }
+    if (msgtype == MSGTYPE_PING) {
+      char sendmsg = MSGTYPE_PING;
+      pthread_mutex_lock(&socket_send_mutex);
+      send(sock1, &sendmsg, 1, 0);
+      pthread_mutex_unlock(&socket_send_mutex);
+
+    } else if (msgtype == MSGTYPE_REQUEST) {
+      int request_id = 0;
+      int block_size = 0;
+
+      recv(sock1, &request_id, sizeof(int), MSG_WAITALL);
+      recv(sock1, &block_size, sizeof(int), MSG_WAITALL);
+
+      request_t* request = malloc(sizeof(request));
+      request->id = request_id;
+      request->size = block_size;
+      request->content = malloc(sizeof(char) * request->size);
+
+      recv(sock1, request->content, request->size, MSG_WAITALL);
+
+      pthread_mutex_lock(&queue_mutex);
+      queadd(&requests, request);
+      pthread_cond_broadcast(&queue_new_element_cond);
+      pthread_mutex_unlock(&queue_mutex);
+
+      printf("Got all blocks!\n");
+    }
+  }
+
+  close(sock1);
 
   return 0;
 }
--- cw10/zad1/server.c
+++ cw10/zad1/server.c
@@ -2,34 +2,110 @@
 #include <errno.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include "errors.h"
+#include "utils.h"
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 #define PORT 5555
 #define MAXMSG 512
 
-int tcp = 0;
+#define MODE_TCP 1
+#define MODE_UDP 0
 
-int read_from_client(int filedes) {
-  char buffer[MAXMSG];
+#define CLIENT_ID_SIZE 10
+#define CLIENTS_SIZE 1000
+
+#define CLIENT_STATE_NULL 0
+#define CLIENT_STATE_BUSY 1
+#define CLIENT_STATE_IDLE 2
+
+int request_counter = 0;
+
+void timeval_diff(struct timeval* start,
+                  struct timeval* stop,
+                  struct timeval* diff) {
+  if ((stop->tv_usec - start->tv_usec) < 0) {
+    diff->tv_sec = stop->tv_sec - start->tv_sec - 1;
+    diff->tv_usec = 1000000 + stop->tv_usec - start->tv_usec;
+  } else {
+    diff->tv_sec = stop->tv_sec - start->tv_sec;
+    diff->tv_usec = stop->tv_usec - start->tv_usec;
+  }
+}
+
+int sock1;
+int sock2;
+fd_set active_fd_set, read_fd_set;
+
+typedef struct client_t {
+  char id[CLIENT_ID_SIZE];
+  int state;
+  struct timeval last_ping_time;
+  int sockid;
+} client_t;
+
+client_t** clients;
+
+int mode = MODE_TCP;
+pthread_mutex_t socket_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+client_t* get_client_with_socket_id(int sockid) {
+  client_t* client = NULL;
+  for (int j = 0; j < CLIENTS_SIZE; j++)
+    if (clients[j]->sockid == sockid) {
+      client = clients[j];
+      break;
+    }
+  return client;
+}
+#define MSGTYPE_PING 1
+#define MSGTYPE_RESULT 2
+#define MSGTYPE_REQUEST 3
+int read_from_client(client_t* client) {
+  char msgtype;
   int nbytes;
 
-  nbytes = read(filedes, buffer, MAXMSG);
-  if (nbytes < 0) {
-    /* Read error. */
-    perror("read");
-    exit(EXIT_FAILURE);
-  } else if (nbytes == 0)
-    /* End-of-file. */
+  nbytes = read(client->sockid, &msgtype, 1);
+  if (nbytes <= 0) {
     return -1;
-  else {
-    /* Data read. */
-    fprintf(stderr, "Server: got message: `%s'\n", buffer);
+  } else {
+    if (msgtype == MSGTYPE_PING) {
+      gettimeofday(&client->last_ping_time, NULL);
+    } else if (msgtype == MSGTYPE_RESULT) {
+      printf("Result\n");
+
+      int response_id = 0;
+      recv(client->sockid, &response_id, sizeof(int), MSG_WAITALL);
+      int words_len = 0;
+      recv(client->sockid, &words_len, sizeof(int), MSG_WAITALL);
+      int response_size = 0;
+      recv(client->sockid, &response_size, sizeof(int), MSG_WAITALL);
+      char* response = malloc(sizeof(char) * response_size);
+      char* response_pointer = response;
+      int remainging_bytes = response_size;
+
+      while (remainging_bytes > 0) {
+        int nbytes = recv(client->sockid, response_pointer,
+                          MIN(remainging_bytes, 1024), MSG_WAITALL);
+        response_pointer += nbytes;
+        remainging_bytes -= nbytes;
+      }
+      printf("Response words: %s", response);
+      printf("Response [%d]: word count: %d\n", response_id, words_len);
+
+      free(response);
+    }
     return 0;
   }
 }
@@ -39,7 +115,7 @@ int make_inet_socket(uint16_t port) {
   struct sockaddr_in name;
 
   /* Create the socket. */
-  if (tcp) {
+  if (mode == MODE_TCP) {
     sock = socket(PF_INET, SOCK_STREAM, 0);
 
   } else {
@@ -64,18 +140,18 @@ int make_inet_socket(uint16_t port) {
   return sock;
 }
 
-int make_unix_socket(uint16_t port) {
+int make_unix_socket(char* path) {
   int sock;
   struct sockaddr_un name;
 
-  /* Create the socket. */
-  /* Create the socket. */
-  if (tcp) {
+  if (mode == MODE_TCP) {
     sock = socket(PF_UNIX, SOCK_STREAM, 0);
 
   } else {
     sock = socket(PF_UNIX, SOCK_DGRAM, 0);
   }
+  int true = 1;
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &true, sizeof(int));
   if (sock < 0) {
     perror("socket");
     exit(EXIT_FAILURE);
@@ -83,8 +159,8 @@ int make_unix_socket(uint16_t port) {
 
   /* Give the socket a name. */
   name.sun_family = AF_UNIX;
-  strcpy(name.sun_path, "/tmp/socket");
-  unlink("/tmp/socket");
+  strcpy(name.sun_path, path);
+  unlink(path);
   if (bind(sock, (struct sockaddr*)&name, sizeof(name)) < 0) {
     perror("bind2");
     exit(EXIT_FAILURE);
@@ -93,20 +169,130 @@ int make_unix_socket(uint16_t port) {
   return sock;
 }
 
-int main(int argc, char* argv[]) {
-  int sock1;
-  int sock2;
+void* ping_thread_fun(void* data) {
+  while (1) {
+    sleep(2);
+    struct timeval actual_time;
+    gettimeofday(&actual_time, NULL);
+    struct timeval diff;
+    for (int j = 0; j < CLIENTS_SIZE; j++)
+      if (clients[j]->state != CLIENT_STATE_NULL) {
+        timeval_diff(&clients[j]->last_ping_time, &actual_time, &diff);
+        if (diff.tv_sec > 5) {
+          close(clients[j]->sockid);
+          FD_CLR(clients[j]->sockid, &active_fd_set);
+          clients[j]->state = CLIENT_STATE_NULL;
+        }
+      }
 
-  fd_set active_fd_set, read_fd_set;
+    pthread_mutex_lock(&socket_mutex);
+    for (int i = 0; i < FD_SETSIZE; i++) {
+      if (FD_ISSET(i, &active_fd_set)) {
+        if (i == sock1 || i == sock2) {
+        } else {
+          char msgsend = MSGTYPE_PING;
+          send(i, &msgsend, 1, 0);
+        }
+      }
+    }
+    pthread_mutex_unlock(&socket_mutex);
+  }
+
+  return NULL;
+}
+
+int send_request(char* path) {
+  printf("%s a\n", path);
+  FILE* f = fopen(path, "r");
+  fseek(f, 0, SEEK_END);
+  long fsize = ftell(f);
+  fseek(f, 0, SEEK_SET);
+
+  char* string = malloc(fsize + 1);
+  fread(string, 1, fsize, f);
+  fclose(f);
+  string[fsize] = 0;
+
+  int blocks_num = 0;
+  int BUFFER_SIZE = 1024;
+  int pointer = 0;
+  while (pointer != fsize) {
+    int old_pointer = pointer;
+    int end_pointer = old_pointer + BUFFER_SIZE;
+    if (end_pointer >= fsize) {
+      end_pointer = fsize;
+    } else {
+      while (string[end_pointer] != ' ')
+        end_pointer--;
+    }
+    blocks_num++;
+    pointer = end_pointer;
+  }
+
+  pthread_mutex_lock(&clients_mutex);
+  client_t* client = NULL;
+  for (int j = 0; j < CLIENTS_SIZE; j++) {
+    if (clients[j]->state == CLIENT_STATE_IDLE) {
+      client = clients[j];
+      break;
+    }
+  }
+
+  if (client == NULL) {
+    for (int j = 0; j < CLIENTS_SIZE; j++) {
+      if (clients[j]->state != CLIENT_STATE_NULL) {
+        client = clients[j];
+        break;
+      }
+    }
+  }
+  if (client != NULL) {
+    client->state = CLIENT_STATE_BUSY;
+  }
+  pthread_mutex_unlock(&clients_mutex);
+
+  if (client == NULL) {
+    return -1;
+  }
+
+  pthread_mutex_lock(&socket_mutex);
+
+  char type = MSGTYPE_REQUEST;
+  request_counter++;
+  printf("elo\n");
+  send(client->sockid, &type, 1, 0);
+  send(client->sockid, &request_counter, sizeof(int), 0);
+  send(client->sockid, &fsize, sizeof(int), 0);
+  send(client->sockid, string, fsize, 0);
+
+  pthread_mutex_unlock(&socket_mutex);
+
+  free(string);
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 3)
+    return err("Missing arguments <port> <unix_path>", -1);
+
+  int port = 0;
+  if (parse_int(argv[1], &port) < 0)
+    return err("Wrong port", -1);
+
+  clients = malloc(sizeof(client_t*) * CLIENTS_SIZE);
+  for (int i = 0; i < CLIENTS_SIZE; i++) {
+    clients[i] = malloc(sizeof(client_t) * CLIENTS_SIZE);
+    clients[i]->state = CLIENT_STATE_NULL;
+  }
+
   int i;
   struct sockaddr_in clientname;
   socklen_t size;
 
   /* Create the socket and set it up to accept connections. */
-  sock1 = make_inet_socket(PORT);
-  sock2 = make_unix_socket(PORT);
+  sock1 = make_inet_socket(port);
+  sock2 = make_unix_socket(argv[2]);
 
-  if (tcp)
+  if (mode == MODE_TCP)
     if (listen(sock1, 1) < 0 || listen(sock2, 1) < 0) {
       perror("listen");
       exit(EXIT_FAILURE);
@@ -114,9 +300,14 @@ int main(int argc, char* argv[]) {
 
   /* Initialize the set of active sockets. */
   FD_ZERO(&active_fd_set);
+  FD_SET(STDIN_FILENO, &active_fd_set);
   FD_SET(sock1, &active_fd_set);
   FD_SET(sock2, &active_fd_set);
 
+  pthread_t id;
+  int data = 0;
+  int res = pthread_create(&id, NULL, ping_thread_fun, &data);
+  printf("%d\n", res);
   while (1) {
     /* Block until input arrives on one or more active sockets. */
     read_fd_set = active_fd_set;
@@ -128,8 +319,15 @@ int main(int argc, char* argv[]) {
     /* Service all the sockets with input pending. */
     for (i = 0; i < FD_SETSIZE; ++i)
       if (FD_ISSET(i, &read_fd_set)) {
-        if (i == sock1 || i == sock2) {
-          if (tcp) {
+        if (i == STDIN_FILENO) {
+          char path[128];
+          size_t sz = read(0, path, sizeof(path));
+          path[sz - 1] = '\0';
+          send_request(path);
+        } else if (i == sock1 || i == sock2) {
+          pthread_mutex_lock(&socket_mutex);
+
+          if (mode == MODE_TCP) {
             /* Connection request on original socket. */
             size_t new;
             size = sizeof(clientname);
@@ -138,25 +336,68 @@ int main(int argc, char* argv[]) {
               perror("accept");
               exit(EXIT_FAILURE);
             }
+
+            struct timeval tv;
+            tv.tv_sec = 1;
+            tv.tv_usec = 0;
+            setsockopt(new, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv,
+                       sizeof tv);
+
             if (i == sock1)
               fprintf(stderr, "Server: connect from host %s, port %hd.\n",
                       inet_ntoa(clientname.sin_addr),
                       ntohs(clientname.sin_port));
             FD_SET(new, &active_fd_set);
+
+            int found_index = -1;
+            for (int j = 0; j < CLIENTS_SIZE; j++)
+              if (clients[j]->state == CLIENT_STATE_NULL) {
+                gettimeofday(&clients[j]->last_ping_time, NULL);
+                clients[j]->sockid = new;
+                found_index = j;
+                break;
+              }
+            if (found_index == -1) {
+              printf("Exceeded clients size\n");
+              exit(2137);
+            }
+
+            int nbytes = recv(new, clients[found_index]->id, CLIENT_ID_SIZE, 0);
+            if (nbytes > 0) {
+              printf("Added new client\n");
+              clients[found_index]->state = CLIENT_STATE_IDLE;
+            } else {
+              printf("Error adding new client\n");
+            }
+
+            // char type = MSGTYPE_REQUEST;
+            // send(new, &type, 1, 0);
+            // int num = 0;
+            // send(new, &num, sizeof(int), 0);
+            // num = 2;
+            // send(new, &num, sizeof(int), 0);
+            // num = 6;
+            // send(new, &num, sizeof(int), 0);
+            // send(new, "BLOCK1", 6, 0);
+            // send(new, &num, sizeof(int), 0);
+            // send(new, "BLOCK2", 6, 0);
+
             if (i == sock2)
               printf("Added new unix socket\n");
           } else {
-            read_from_client(i);
+            // read_from_client(i);
           }
+          pthread_mutex_unlock(&socket_mutex);
 
-        } else if (i == sock2) {
-          read_from_client(i);
         } else {
           /* Data arriving on an already-connected socket. */
-          printf("New data\n");
-          if (read_from_client(i) < 0) {
+          client_t* client = get_client_with_socket_id(i);
+
+          if (read_from_client(client) < 0) {
             close(i);
+            printf("Client [%s] disconnected\n", client->id);
             FD_CLR(i, &active_fd_set);
+            client->state = CLIENT_STATE_NULL;
           }
         }
       }
